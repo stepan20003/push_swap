2fe4bb7808b57725070bab55f2a6b7e7e494f002
Stepan Stepanyan <ststepan@c2r8s3.42yerevan.am>
2026-03-22 16:32:07 +0400

    donee

---
+++ complex_sort.c
@@ -0,0 +1,67 @@
+#include "push_swap.h"
+
+static void	assign_ranks(t_stack *a)
+{
+	t_stack	*i;
+	t_stack	*j;
+	int		rank;
+
+	i = a;
+	while (i)
+	{
+		rank = 0;
+		j = a;
+		while (j)
+		{
+			if (j->value < i->value)
+				rank++;
+			j = j->next;
+		}
+		i->rank = rank;
+		i = i->next;
+	}
+}
+
+static int	max_bits(int size)
+{
+	int	bits;
+
+	bits = 0;
+	while ((1 << bits) < size)
+		bits++;
+	return (bits);
+}
+
+static void	radix_pass(t_stack **a, t_stack **b, int bit, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (((*a)->rank >> bit) & 1)
+			funct_ra(a);
+		else
+			funct_pb(a, b);
+		i++;
+	}
+	while (*b)
+		funct_pa(a, b);
+}
+
+void	complex_sort(t_stack **a, t_stack **b)
+{
+	int	size;
+	int	bits;
+	int	bit;
+
+	assign_ranks(*a);
+	size = stack_size(*a);
+	bits = max_bits(size);
+	bit = 0;
+	while (bit < bits)
+	{
+		radix_pass(a, b, bit, size);
+		bit++;
+	}
+}
